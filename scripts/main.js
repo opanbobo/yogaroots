da98fe42bca9ec84428b37755bb9f6d39c316c22
Tofan <tofan.rukmana@bankina.id>
2023-08-23 15:30:34 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
-console.log('\'Allo \'Allo!');
+import 'bootstrap';
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
-
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+import {$,jQuery} from 'jquery';
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
