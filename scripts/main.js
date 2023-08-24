ec6f37636ae302c9b420d5edbe559218d00dadd8
Tofan <tofan.rukmana@bankina.id>
2023-08-24 10:26:16 +0700

    update about page done

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,23 @@
-// header
-const header = document.querySelector("#header");
-const toggleClass = "scroll-head";
+$( document ).ready(function() {
 
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll > 100) {
-    header.classList.add(toggleClass);
-  } else {
-    header.classList.remove(toggleClass);
-  }
+  // header
+  const header = document.querySelector("#header");
+  const toggleClass = "scroll-head";
+
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > 100) {
+      header.classList.add(toggleClass);
+    } else {
+      header.classList.remove(toggleClass);
+    }
+  });
+
+  // slider
+  $('.single-item').slick({
+    arrows: true,
+            prevArrow:"<div type='button' class='slick-prev arrows pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></div>",
+            nextArrow:"<div type='button' class='slick-next arrows pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></div>"
+  });
+
 });
